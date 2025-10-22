040356b8b33876ae5efed475b24ba914276251bf
AdilZubairWeb <168962761+AdilZubairWeb@users.noreply.github.com>
2025-10-22 12:52:04 +0530

    Adjust project URL save handling

--- src/NextOnServices.WebUI/wwwroot/customJS/ProjectsURL/ProjectsURL.js
+++ src/NextOnServices.WebUI/wwwroot/customJS/ProjectsURL/ProjectsURL.js
@@ -81,7 +81,8 @@ function AddURL() {
             UnblockUI();
             Swal.fire({ title: 'Success!', text: "Project Added", icon: 'success', confirmButtonText: 'OK' }).then((result) => {
                 if (result.isConfirmed) {
-                    window.location.href = "/VT/Projects/ProjectsList";
+                    AddProjectURLPartialView();
+                    ListProjectURLPartialView();
                 }
             });
         },
